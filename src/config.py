2ce36ea67b2a28b910a8acedb8307500a0028a35
Pascal Herrmann <pascal.herrmann@tum.de>
2020-06-18 03:23:13 +0200

    first try

--- src/config.py
+++ src/config.py
@@ -26,7 +26,7 @@ DESC = ""
 DATA_DIR = "/content/gdrive/My Drive/Public/tensorboards_shared/dataset_tfrecords/dataset_tfrecords1-r07.tfrecords" # must be copied!
 PICKLE_DIR = "/content/gdrive/My Drive/Public/tensorboards_shared/Training_Decoder_TF/00000-sgan-ffhq128-1gpu/network-snapshot-011489.pkl"
 
-ENCODER_PICKLE_DIR = None
+ENCODER_PICKLE_DIR = "/content/gdrive/My Drive/Public/tensorboards_shared/run/15_both_encoder_conditional_discriminator/snapshots/network-snapshot-01300160.pkl"
 # Encoder
 GDRIVE_PATH = "/content/gdrive/My Drive/Public/tensorboards_shared"
 
